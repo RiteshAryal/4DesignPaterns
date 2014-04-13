3b7412f87ff45e29f52e27d03fab9a756c83172b
Ritesh <ritesh.aryal@nccgroup.com>
2014-04-13 19:57:54 +0100

    strategy code cleanup

--- strategy/DuckApp/index.php
+++ strategy/DuckApp/index.php
@@ -1,6 +1,11 @@
 <?php
 include 'MallardDuck.php';
+
+use strategy\DuckApp\flybehavior\FlyWithWings;
 use strategy\DuckApp\MallardDuck;
 
 $mallardDuck = new MallardDuck();
 $mallardDuck->display();
+
+$mallardDuck->performFly(new FlyWithWings());
+$mallardDuck->performQuack(new strategy\DuckApp\quackbehavior\Quack());  
